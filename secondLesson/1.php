de39a0df5eca95cf92859b5a25fef6167a7cb8a5
NeOWkA <patcvich@gmail.com>
2022-01-31 13:34:42 +0300

    added new files

--- secondLesson/1.php
+++ secondLesson/1.php
@@ -1,13 +1,11 @@
 <?php
 $a = rand(-100, 100);
 $b = rand(-100, 100);
-if ($a >= 0 && $b >=0) {
+if ($a >= 0 && $b >= 0) {
     $result = "a = {$a},  b = {$b}, оба положительные";
-}
-elseif ($a < 0 && $b <0) {
+} elseif ($a < 0 && $b < 0) {
     $result = "a = {$a},  b = {$b}, оба отрицательные";
-}
-else {
+} else {
     $result = "a = {$a},  b = {$b}, разных знаков";
 }
 echo $result;
