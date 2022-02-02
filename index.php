4545317e25b8bb584406a239c46e97908bd4aa44
NeOWkA <patcvich@gmail.com>
2022-02-02 13:43:01 +0300

    added new files

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+include "classSimpleImage.php";
+
 define("ROOT", $_SERVER['DOCUMENT_ROOT']);
 define("IMG_BIG", ROOT . "/gallery_img/big/");
 define("IMG_SMALL", ROOT . "/gallery_img/small/");
@@ -11,23 +13,20 @@ function getImages($path)
 $images = getImages(IMG_BIG);
 
 if (!empty($_FILES)) {
-    var_dump($_FILES['myFile']);
     $path_big = IMG_BIG . $_FILES['myFile']['name'];
     $path_small = IMG_SMALL . $_FILES['myFile']['name'];
-    var_dump($path_big);
-    var_dump($path_small);
     if (move_uploaded_file($_FILES['myFile']['tmp_name'], $path_big)) {
+        $message = "Downloaded";
         $image = new SimpleImage();
         $image->load($path_big);
         $image->resizeToWidth(150);
         $image->save($path_small);
         header("Location: /");
     } else {
-        echo "Error<br>";
+        $message = "Error";
         die();
     }
 }
 $images = getImages(IMG_BIG);
 
-include "classSimpleImage.php";
 include "mygallery.php";
--- mygallery.php
+++ mygallery.php
@@ -20,6 +20,7 @@
 </head>
 
 <body>
+<?=$message?><br>
 <div id="main">
     <div class="post_title"><h2>Моя галерея</h2></div>
     <div class="gallery">
